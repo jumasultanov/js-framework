1ef89804b90f109dffa02f2e31b2483d572f86c8
jumasultanov <jumasultanov@rambler.ru>
2022-03-18 23:00:50 +0500

    add method range

--- App/Core/Services/Helpers/Helper.js
+++ App/Core/Services/Helpers/Helper.js
@@ -1,7 +1,25 @@
 class Helper {
 
+    /**
+     * Возвращает объект - дескриптор
+     */
     static getDescriptor(value, configurable = true, writable = true, enumerable = true) {
         return { value, configurable, writable, enumerable }
+    }
+
+    /**
+     * Возвращает массив чисел, начиная от start
+     * @param {number} start Начальное число
+     * @param {number} length Кол-во чисел
+     * @returns {number[]}
+     */
+    static range(start, length) {
+        if (length < 1) return [];
+        return [...(function* r(start, end) {
+            yield start;
+            if (start === end) return;
+            yield* r(start + 1, end);
+        })(start, start + length - 1)];
     }
 
 }
