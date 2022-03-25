668d6c967521a204b94c1cdf4faaa891738861a3
jumasultanov <jumasultanov@rambler.ru>
2022-03-25 14:20:16 +0500

    modified directive FOR + adaptive for objects

--- App/Core/Directives/For.js
+++ App/Core/Directives/For.js
@@ -15,6 +15,7 @@ class For {
     static ACTION_MOVE = 2;
     static ACTION_REVERSE = 3;
     static ACTION_SORT = 4;
+    static ACTION_REPLACE = 5;
     //Методы перехвата
     static methods = [ 'push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse' ];
     //Объект из прототипа массива
@@ -50,8 +51,11 @@ class For {
                     let len = arguments.length;
                     let arrLen = this.length;
                     while (len--) args[len] = arguments[len];
+                    //Пропускаем обновления
+                    For.skipUpdate = true;
                     //Вызываем оригинальный метод
                     let result = original.apply(this, args);
+                    For.skipUpdate = false;
                     //Вызываем метод перехватчик
                     func.call(For, this, arrLen, args);
                     return result;
@@ -93,8 +97,7 @@ class For {
         if (!inserted.length) return;
         let keys = Helper.range(0, inserted.length);
         this.arrayChange(target, [0, inserted.length, prevLength], this.ACTION_MOVE);
-        this.arrayChange(target, keys, this.ACTION_PUSH);
-        this.movedComp = null;
+        this.arrayChange(target, { keys, before: 0 }, this.ACTION_PUSH);
     }
 
     /**
@@ -112,7 +115,6 @@ class For {
         if (prevLength < 1) return;
         this.arrayChange(target, 0, this.ACTION_DELETE);
         this.arrayChange(target, [1, 0, target.length], this.ACTION_MOVE);
-        this.movedComp = null;
     }
 
     /**
@@ -135,14 +137,16 @@ class For {
         //Сдвигаем, если сдвигается и существующий индекс
         let diff = inserted.length - deleteCount;
         let moveIndex = index + deleteCount;
+        let before = moveIndex;
         if (diff !== 0 && moveIndex < prevLength) {
             let newIndex = moveIndex + diff;
+            before = newIndex;
             this.arrayChange(target, [moveIndex, newIndex, prevLength - moveIndex], this.ACTION_MOVE);
         }
         //Добавляем, если есть что добавлять
         if (inserted.length) {
             let keys = Helper.range(index, inserted.length);
-            this.arrayChange(target, keys, this.ACTION_PUSH);
+            this.arrayChange(target, { keys, before }, this.ACTION_PUSH);
         }
     }
 
@@ -237,12 +241,21 @@ class For {
                     this.componentChange(params);
                     if (count > 0) return;
                 } else {
+                    //Если уже есть список добавленных, то очищаем его
+                    // TODO: При замене объекта или массива, нужно сравнивать элементы массива, чтобы удалить те, которые отсутствуют
+                    //          Потом вставить список компонентов, которых не хватает, допустим перезаписали массив с помощью filter и т.д.
+                    if (vnode.data.inserted.size) {
+                        vnode.component.clearChildren(vnode.data.inserted, true);
+                    }
                     //Добавляем флаг и отслеживаем, что объект будет проитерирован
                     this.setRroto(data.current);
                     //Добавляем скрытый метод getWatcher, которая будет вести до родителя оригинального объекта
                     // TODO: data.expr лучше не использовать, т.к. в выражение может указана операция, типа вызов функции
+                    //  Если уже есть, то не добавлять, хотя зависит от того, куда добавляется
                     const parent = Area.getOwnKey(data.component.path.slice(0, -1), data.expr);
                     if (parent) AreaExpanding.setWatcher(parent.vars, data.expr);
+                    //Если объект, то наблюдаем за добавлениями и удалениями
+                    this.changeWatch(data, vnode.component, this.namePrefix, data.current.isArray);
                     //Обновляем весь список
                     if (count) return this.componentPush(Object.keys(data.current));
                 }
@@ -252,6 +265,35 @@ class For {
                 vnode.component.replaceChildren([forElse.component], vnode.data.space, vnode.data.inserted);
             }
         });
+    }
+
+    /**
+     * Добавление наблюдателей для объекта
+     * @param {object} data Данные конструкции цикла
+     * @param {Component} parentComp Родительский компонент
+     * @param {string} namePrefix Префикс конструкции
+     */
+    static changeWatch(data, parentComp, namePrefix, isArray) {
+        data.current.getHandler().addObjectWatchers(
+            isArray ? null : changeParams => {
+                //Добавляем свойство
+                this.arrayChange(data.current, [changeParams.prop], this.ACTION_PUSH);
+            },
+            isArray ? null : changeParams => {
+                //Удаляем свойство
+                this.arrayChange(data.current, changeParams.prop, this.ACTION_DELETE);
+            },
+            changeParams => {
+                if (this.skipUpdate) return;
+                //Меняем значение в компоненте
+                if (data.current[changeParams.prop] instanceof Object) {
+                    this.arrayChange(data.current, changeParams.prop, this.ACTION_REPLACE);
+                } else {
+                    const comp = parentComp.children[namePrefix + changeParams.prop];
+                    comp.vars[data.as[0]||'item'] = data.current[changeParams.prop];
+                }
+            }
+        );
     }
 
     /**
@@ -279,6 +321,7 @@ class For {
         if (params.change == this.ACTION_REVERSE)   return this.componentReverse();
         if (params.change == this.ACTION_SORT)      return this.componentSort();
         if (params.change == this.ACTION_DELETE)    return this.componentDelete(key);
+        if (params.change == this.ACTION_REPLACE)    return this.componentReplace(key);
     }
 
     /**
@@ -286,16 +329,17 @@ class For {
      * @param {string[]} keys 
      */
     static componentPush(keys) {
+        let before = this.vnode.data.space;
+        //Если передали объект, который содержит индекс, перед которым нужно вставить
+        if (!Array.isArray(keys)) {
+            const comp = this.getComp(keys.before);
+            if (comp) before = comp.element;
+            keys = keys.keys;
+        }
         //Создаем фрагмент и добавляем туда элементы компонентов
         const fragment = Block.getFragment();
         for (const key of keys) {
-            this.componentInsert(key, fragment);
-        }
-        let before = this.vnode.data.space;
-        //Если был сдвиг, то вставка будет перед началом сдвига
-        if (this.movedComp) {
-            before = this.movedComp.element;
-            this.movedComp = null;
+            this.componentInsert(key, fragment, true);
         }
         //Вставка фрагмента перед before
         Block.insert(fragment, before);
@@ -317,13 +361,13 @@ class For {
             //Перебираем с конца вниз
             for (let i = limits[2] - 1; i >= 0; i--) {
                 const newK = limits[0] + diff + i;
-                this.componentRename(limits[0] + i, newK, i === 0);
+                this.componentRename(limits[0] + i, newK);
             }
         } else {
             //Перебираем с начала вверх
             for (let i = 0; i < limits[2]; i++) {
                 const newK = limits[0] - diff + i;
-                this.componentRename(limits[0] + i, newK, i === 0);
+                this.componentRename(limits[0] + i, newK);
             }
         }
     }
@@ -332,22 +376,19 @@ class For {
      * Переименовывание компонента
      * @param {number} fromKey Перемещаемый ключ
      * @param {number} toKey Новый ключ
-     * @param {boolean} saveComp Если нужно сохранить компонент
      */
-    static componentRename(fromKey, toKey, saveComp = false) {
+    static componentRename(fromKey, toKey) {
         //Ищем и переименовываем компонент и данные
-        const comp = this.vnode.component.children[this.namePrefix + fromKey];
+        const comp = this.getComp(fromKey);
         if (!comp) return;
         this.vnode.component.renameChild(comp, this.namePrefix + toKey);
         //Меняем индекс в данных компонента
         comp.vars[this.data.as[1]||'key'] = String(toKey);
-        //Храним компонент для последующих вставок перед ним
-        if (saveComp) this.movedComp = comp;
     }
 
     /**
      * Удаление компонента
-     * @param {string|string[]} key Название компонента
+     * @param {string|string[]} key 
      */
     static componentDelete(key) {
         //Если передан массив ключей
@@ -356,7 +397,7 @@ class For {
             return;
         }
         //Ищем компонент по ключу и удаляем
-        const comp = this.vnode.component.children[this.namePrefix + key];
+        const comp = this.getComp(key);
         if (!comp) return;
         this.vnode.component.removeChild(comp, this.vnode.data.inserted, true);
     }
@@ -365,10 +406,28 @@ class For {
      * Создание и вставка компонента по ключу
      * @param {string} key 
      * @param {Node} space Элемент, внутрь которого будет вставлено
+     * @param {boolean} append Если нужно вставить внутрь space
      */
-    static componentInsert(key, space) {
-        const comp = this.data.component.clone(this.namePrefix + key, true, this.getObjectForCycle(this.data, key));
-        this.vnode.component.insertChild(comp, space, this.vnode.data.inserted, true);
+    static componentInsert(key, space, append = false) {
+        const data = this.data;
+        //Клонируем из базового компонента с данными итерации
+        const comp = data.component.clone(this.namePrefix + key, true, this.getObjectForCycle(data, key));
+        //Актививуем его и вставляем
+        this.vnode.component.insertChild(comp, space, this.vnode.data.inserted, append);
+    }
+
+    /**
+     * Пересоздание компонента по ключу
+     * @param {string} key 
+     */
+    static componentReplace(key) {
+        //Получаем элемент, перед которым будем вставлять
+        let before = this.vnode.data.space;
+        const comp = this.getComp(key);
+        if (comp) before = comp.element.nextElementSibling;
+        //Удаляем компонент и добавляем
+        this.componentDelete(key);
+        this.componentInsert(key, before);
     }
 
     /**
@@ -379,14 +438,14 @@ class For {
         const half = Math.ceil((len - 1) / 2);
         //С предпоследнего элемента идем наверх и перемещаем перед спец.элементом
         for (let i = len - 1; i >= 0; i--) {
-            const comp = this.vnode.component.children[this.namePrefix + i];
+            const comp = this.getComp(i);
             if (!comp) continue;
             Block.insert(comp.element, this.vnode.data.space);
             //Если дошли до оставшейся половины, то меняем данные компонентов с противоположными
             if (i < half) {
                 //Ищем нужный компонент
                 const replaceKey = len - 1 - i;
-                const replace = this.vnode.component.children[this.namePrefix + replaceKey];
+                const replace = this.getComp(replaceKey);
                 if (replace) {
                     //Меняем компоненты
                     this.vnode.component.swapChild(replace, comp);
@@ -405,13 +464,13 @@ class For {
         const item = this.data.as[0]||'item';
         //Начинаем проверять в конца
         for (let i = len - 1; i >= 0; i--) {
-            const comp = this.vnode.component.children[this.namePrefix + i];
+            const comp = this.getComp(i);
             if (!comp) continue;
             const value = this.data.current[i];
             //Если значения не совпадают, то ищем выше до этого индекса
             if (comp.vars[item] !== value) {
                 for (let j = 0; j < i; j++) {
-                    const replace = this.vnode.component.children[this.namePrefix + j];
+                    const replace = this.getComp(j);
                     if (!replace) continue;
                     //Если нашлось, то меняем местами компоненты и блоки
                     if (value === replace.vars[item]) {
@@ -423,6 +482,15 @@ class For {
                 }
             }
         }
+    }
+
+    /**
+     * Возвращает компонент по ключу итерации
+     * @param {string} key 
+     * @returns {Component}
+     */
+    static getComp(key) {
+        return this.vnode.component.children[this.namePrefix + key];
     }
 
     /**
